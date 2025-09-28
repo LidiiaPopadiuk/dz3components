056c6fff294f744f7b36bd40a58c938cc8326ca2
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2025-09-28 18:59:24 +0200

    feat: finish

---
+++ src/components/Staristics.jsx
@@ -0,0 +1,16 @@
+export const Statistics = ({ title, stats }) => {
+    return (
+        <section class="statistics">
+            {title && <h2 class="title">{title}</h2>}
+
+            <ul class="stat-list">
+                {stats.map(({id, label, percentage}) => (
+                     <li key={id}>
+                        <span>{label}</span>{' '}
+                        <span>{percentage}%</span>
+                     </li>
+                ))}
+            </ul>
+        </section>
+    )
+}
---
+++ src/components/exercise3/Friends.jsx
@@ -0,0 +1,18 @@
+import {FriendListItem} from './FriendListItem'
+
+export const FriendList = ({ friends }) => {
+    return (
+        <ul class="friend-list">
+         <>
+         {friends.map(({id, avatar, name, isOnline}) => (
+            <FriendListItem 
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            />
+         ))}
+         </>
+        </ul>
+    )
+}
